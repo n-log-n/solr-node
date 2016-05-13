c49d585c0e8c72452fd35d57506c8bef2491dd3e
Donghyun Go <godong9@gmail.com>
2016-05-13 10:22:10 +0900

    Support #45
    - Support setDebugLevel

--- test/query-test.js
+++ test/query-test.js
@@ -9,6 +9,27 @@ var expect = require('chai').expect,
   Query = require('../lib/query');
 
 describe('Query', function() {
+  describe('#constructor', function() {
+    it('should create default query.', function() {
+      //given
+      //when
+      var testQuery = new Query();
+      //then
+      expect(testQuery.params).to.eql([]);
+    });
+
+    it('should set logger query.', function() {
+      //given
+      var options = {
+        setDebugLevel: 'ALL'
+      };
+      //when
+      var testQuery = new Query(options);
+      //then
+      expect(testQuery.params).to.eql([]);
+    });
+  });
+
   describe('#dismax', function() {
     it('should get dismax params.', function() {
       //given
