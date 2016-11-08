f94f957934f8a259ed131f216dddc20de3baf97a
Greg Parsons <gregorskii@gmail.com>
2016-11-08 10:09:05 -0800

    adds support for spatial params

--- test/query-test.js
+++ test/query-test.js
@@ -172,6 +172,19 @@ describe('Query', function() {
     });
   });
 
+  describe('#spatial', function() {
+    it('should get spatial params.', function() {
+      var testQuery = new Query();
+      testQuery.spatial({
+        pt: '33.9609853,-118.3383601',
+        sfield: 'latlong',
+        d: '50'
+      });
+
+      expect(testQuery.params).to.eql([ 'spatial=true', "pt=33.9609853%2C-118.3383601", 'sfield=latlong', 'd=50' ]);
+    });
+  });
+
   describe('#_getFqStr', function() {
     it('should get fqStr when field exist.', function() {
       //given
